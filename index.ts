25855c65fec3530872b1f4beabe26722a75b9e7a
Ed Opare-Aryee <ed@thisissoon.com>
2017-12-04 10:10:55 +0000

    feat(build): Move build and dev environment to angular cli and ng-packagr
    Simplified the dev and build environment to be based on the cli

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { TimeSinceModule } from './src/app/time-since/time-since.module';
+export { TimeSincePipe } from './src/app/time-since/time-since.pipe';
+export {
+  millisecondsInSecond, secondsInDay, secondsInHour,
+  secondsInMin, secondsInMonth, secondsInYear
+} from './src/app/time-since/shared/durations';
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { TimeSinceModule } from './time-since';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TimeSinceModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
